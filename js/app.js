3f4b4f8ce2c2b43425e639fe0eb1327b70b5aa59
Voligo <fenglichao41@gmail.com>
2017-08-12 16:47:01 +0800

    11

--- js/app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.querySelectorAll('.post-shownotes a').forEach(function(anchor) {
-    anchor.setAttribute('target', '_blank');
-  })
-});
